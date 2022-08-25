70d175c0232b19edba119bb6ff9562ae5cd3666a
Zach Mccain <mccainz@gmail.com>
2022-08-25 13:41:46 -0500

    Add basic ever-true test for jest confirmation

---
+++ src/tests/helloworld.test.ts
@@ -0,0 +1,7 @@
+import {describe, expect, test} from '@jest/globals'
+
+describe('testing Dice rolls', () => {
+    test('Testing Jest integration...', () => {
+            expect('hello world').toEqual('hello world');
+    });
+});
